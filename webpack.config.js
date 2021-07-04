16a669cb57a986b6500ffbebbb8aaed10720da47
Hank-mbp <cgh20xx@gmail.com>
2021-07-05 01:21:40 +0800

    設定 asset-modeules rules，讓 webpack 看懂圖片

--- webpack.config.js
+++ webpack.config.js
@@ -17,25 +17,34 @@ module.exports = {
   },
   devtool: 'source-map',
   plugins: [
+    // 動態生成html，輸出到dist (因為hash後js、css每次檔名都不同，所以要動態生成就不用手動改檔名。)
     new HtmlWebpackPlugin({
-      template: './base.html',
+      template: './src/base.html',
     }),
+    // 把 css 抽離，輸出到 dist
     new MiniCssExtractPlugin({
       filename: 'bundle.[name].[hash].css',
     }),
   ],
   module: {
     rules: [
+      // css 相關 loader 執行順序由右到左 sass-loader > css-loader > MiniCssExtractPlugin
       {
         test: /\.(css|scss)$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      // babel 相關的 rules (需手動建立 babel.config.json)
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
         },
+      },
+      // 讓 webpack 看懂圖片: https://webpack.docschina.org/guides/asset-modules/
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
